4f8f0dcede25b919000129f625a676c6d1cdbaa5
Jon Huynh <jhsoccer123@gmail.com>
2019-01-03 01:19:50 -0500

    Created UICollectionView

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jon Huynh on 1/3/19.
+//  Copyright © 2019 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+}
